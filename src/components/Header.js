528a2ad4611a4f00e7ad68589f23d4dece1b7a1d
rnogara <r.nogara.dev@gmail.com>
2023-04-04 20:10:26 -0300

    Req 7 a 9: Criamos a parte do Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import SearchBar from './SearchBar';
+import SIcon from '../images/searchIcon.svg';
+import PIcon from '../images/profileIcon.svg';
+import { AppContext } from '../context/AppProvider';
 
 function Header() {
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
+  const { helpers: { title } } = useContext(AppContext);
+  const history = useHistory();
+
+  const searchIcon = (
+    <button
+      data-testid="search-top-btn"
+      onClick={ () => setIsSearchClicked(!isSearchClicked) }
+    >
+      {SIcon}
+    </button>
+  );
   return (
     <header>
       <h3>Recipes App</h3>
+      <button
+        data-testid="profile-top-btn"
+        onClick={ () => history.push('/profile') }
+      >
+        {PIcon}
+      </button>
+      { (title === 'Meals' || title === 'Drinks') && searchIcon }
+      <h1 data-testid="page-title">{ title }</h1>
+      { isSearchClicked && <SearchBar /> }
     </header>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <section>
+      <button data-testid="exec-search-btn">Search</button>
+    </section>
+  );
+}
+
+export default SearchBar;
