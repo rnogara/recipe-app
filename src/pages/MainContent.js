ca1482893d18342bd89b3b406d11d2731159a8a0
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-04-05 00:05:21 -0300

    Req 24: Realiza fetch de acordo com o path

--- src/pages/MainContent.js
+++ src/pages/MainContent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import RecipeDetail from '../components/RecipeDetail';
 
 function MainContent() {
   return (
     <section>
       <Header />
+      <RecipeDetail />
     </section>
   );
 }
