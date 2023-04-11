b6a97cf56be297620157043f868a9a3985d72e50
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-04-11 17:06:58 -0300

    Refatoração-Requisitos 34, 35 e 36: Modifica uma importação no arquivo RecipeDetails.js, remove um data-testid duplicado e muda o arquivo ShareAndFav.js para a pasta components. Requisito 38: Cria um helper que converte os detalhes da receita para um objeto que posteriormente será salvo no localStorage e renderiza o componente ShareAndFav. Requisito 39: Cria o arquivo onde serão elaborados os testes.

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { useLocation } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+import { useLocation, useParams } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
-import shareIcon from '../images/shareIcon.svg';
-import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+// import shareIcon from '../images/shareIcon.svg';
+// import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import RecipeInProgressCard from '../components/RecipeInProgressCard';
+import ShareAndFav from '../components/ShareAndFav';
+import { getDetails } from '../helpers/getDetails';
 
 const MEAL_ID_ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 const COCKTAIL_ID_ENDPOINT = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 const INITIAL_IN_PROGRESS_RECIPES = { meals: {}, drinks: {} };
 
-function RecipeInProgress({ match: { params: { id } } }) {
+function RecipeInProgress() {
   const { pathname } = useLocation();
+  const { id } = useParams();
+  const URLpath = window.location.href.replace('/in-progress', '');
   const URL = pathname.includes('meals')
     ? (MEAL_ID_ENDPOINT + id) : (COCKTAIL_ID_ENDPOINT + id);
   const [isLoading, , recipe, fetchRecipe] = useFetch();
@@ -27,10 +31,11 @@ function RecipeInProgress({ match: { params: { id } } }) {
   if (isLoading) return <h1>Loading...</h1>;
   return (
     <section>
-      <button data-testid="share-btn"><img src={ shareIcon } alt="Share" /></button>
+      {/* <button data-testid="share-btn"><img src={ shareIcon } alt="Share" /></button>
       <button data-testid="favorite-btn">
         <img src={ whiteHeartIcon } alt="Favorite" />
-      </button>
+      </button> */}
+      <ShareAndFav url={ URLpath } recipe={ getDetails(recipe, pathname) } />
       { recipe && (
         <RecipeInProgressCard recipe={ recipe } id={ id } />
       )}
@@ -40,11 +45,6 @@ function RecipeInProgress({ match: { params: { id } } }) {
 }
 
 RecipeInProgress.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      id: PropTypes.string,
-    }),
-  }).isRequired,
 };
 
 export default RecipeInProgress;
---
+++ src/tests/RecipeInProgress.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helper/RenderWithRouter';
+import { AppProvider } from '../context/AppProvider';
+import RecipesInProgress from '../pages/RecipesInProgress';
+
+describe('Testando a tela de receitas em progresso', () => {
+  it('', () => {});
+});
