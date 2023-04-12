fde7e5600336beef9bda182521d8922d0c4441bd
gabrielsfdev <ferreirawdev@gmail.com>
2023-04-12 16:30:01 -0300

    Grupo 10 - Req 57-62

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,27 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../components/Header';
 import { AppContext } from '../context/AppProvider';
 
 function Profile() {
   const { functions: { setTitle } } = useContext(AppContext);
+  const [emailState, setEmailState] = useState('loading...');
 
   useEffect(() => {
     setTitle('Profile');
+    const { email } = JSON.parse(localStorage.getItem('user'));
+    setEmailState(email);
   }, []);
 
   return (
     <section>
       <Header />
+      <h3 data-testid="profile-email">
+        { emailState }
+      </h3>
+      <button data-testid="profile-done-btn">Done Recipes</button>
+      <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button data-testid="profile-logout-btn">Logout</button>
+
     </section>
   );
 }
